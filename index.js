4c3716803a9d82d44c6f60d82f0dc5e3695c5a75
iisrax <isra.draw@gmail.com>
2024-04-04 16:44:49 -0300

    testando como funciona

---
+++ index.js
@@ -0,0 +1,17 @@
+// Funções: uma caixinha com diretrizes que executa tudo, quando solicitado...
+function fazer(){
+    console.log("Quando der 19h, regar a plantinha pfvr")
+    regar()
+}
+
+function regar(horario){
+    console.log("Samambaia precisa ser regada")
+
+let horario = "23h"
+if(horario === "19h"){
+    console.log("tá na hora")
+}else if(horario === "20h"){
+    console.log("está atrasado :/")
+}else{
+    console.log("você esqueceu da Samambaia... </3 ")
+}}
