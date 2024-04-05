fe2fd8d17adc6aa0d4c522f8d9c83df66414419b
iisrax <isra.draw@gmail.com>
2024-04-05 16:19:06 -0300

    tentando fixar funções de retorno

---
+++ functionR.js
@@ -0,0 +1,11 @@
+// Funções de Retorno... Jinchuuriki and Bijuus
+let Bijuus = village("Kurama Gyuuki Shukaku")
+let Jinchuuriki = village("Naruto Killer Gaara")
+
+function village(name){
+    let result = name.split(" ")[0]
+
+    return result
+}
+
+console.log("Na vila da Folha o " + Bijuus + " foi selado no " + Jinchuuriki)
